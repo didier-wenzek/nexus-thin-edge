a8a09f2fd0f4cc368099607189f4bc5a96f6e808
Didier Wenzek <didier.wenzek@free.fr>
2021-10-19 17:36:16 +0100

    Basic c8y micro-service to register devices

---
+++ micro-service/application.py
@@ -0,0 +1,42 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+import os
+import random
+import string
+
+app = Flask(__name__)
+
+def random_string(length):
+    return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(length))
+
+# Hello world endpoint
+@app.route('/')
+def hello():
+    return 'Registration service'
+
+# Verify the status of the microservice
+@app.route('/health')
+def health():
+    return '{ "status" : "UP" }'
+
+# Verify the status of the microservice
+@app.route('/register/<device>')
+def register(device):
+    user = f'{device}_{random_string(5)}'
+    password = random_string(20)
+
+
+    command = f'./post-registration.sh {device} {user} {password}'
+    print('command: ', command)
+    res = os.system(command) 
+    print('res: ', res)
+
+    credentials = {
+        'url': 'https://my-ca.com',
+        'user': user,
+        'pass': password,
+    }
+    return jsonify(credentials) 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
